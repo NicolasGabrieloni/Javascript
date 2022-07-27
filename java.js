b8fc920e3278be2c20950a9880ccee9b038bbfa3
Nicolas Gabrieloni <nicogabrieloni@gmail.com>
2022-07-27 11:54:50 -0300

    ciclo for/while

---
+++ java.js
@@ -0,0 +1,11 @@
+let pass = ""
+
+while(pass != "hola"){
+    pass = prompt("Introduzca su contraseña")
+    }
+    console.log("contraseña correcta")
+
+for (var i = 0; i<100; i++){
+    console.log(i);
+    if(i>= 40){break}
+}
