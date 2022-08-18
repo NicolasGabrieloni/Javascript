c41d1d200ab8656f36ba672cda312058c1eeecba
Nicolas Gabrieloni <nicogabrieloni@gmail.com>
2022-08-18 18:53:53 -0300

    agregando eventos

--- variables.js
+++ variables.js
@@ -1,3 +1,21 @@
 const IVA = 1.21
 const productos = []
 const idioma = document.getElementById("tituloh2 tituloNav btnNav th1 th2 th3 th4 btnIdioma")
+
+const botonAgregar = document.querySelector("#btn1")
+const botonQuitar = document.querySelector("#btn2")
+const botonTabla = document.querySelector("#btn3")
+const botonIdioma = document.querySelector("#btnIdioma")
+
+botonAgregar.addEventListener("click", ()=> {
+    agregarProductos()
+})
+botonQuitar.addEventListener("click", ()=> {
+    quitarProductos()
+})
+botonTabla.addEventListener("click", ()=> {
+    cargarTablaProductos()
+})
+botonIdioma.addEventListener("click", ()=> {
+    cambiarIdioma()
+})
